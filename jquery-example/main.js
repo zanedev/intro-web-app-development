af0df6db7873616c05eaa992a18aaa20fcd17a86
zane <zane@zanedev.com>
2014-04-15 10:21:15 -0700

    updates from class

--- jquery-example/main.js
+++ jquery-example/main.js
@@ -1,3 +1,38 @@
+
+//var myvariable = 5;
+//
+//var myFunction = function(cat, dog) {
+//    alert("cat: "+ cat);
+//};
+//
+//var meowfunction = function(){
+////    console.log(arguments);
+//    console.log("hi", arguments);
+//    var newVar = "test";
+//    alert("meow");
+//};
+
+//myFunction("mycat");
+
+//var animalsObject = {
+//    cat:{
+//        name:"ted",
+//        color:"red",
+//        meow: meowfunction
+//    },
+//    dog:"mydog"
+//};
+//
+//var myArray = [
+//    animalsObject,
+//    "test",
+//    "red"
+//];
+
+
+
+
+
 //jquery example
 //cache our book lists dom elements
 var availableBooks = $('#available-books');
@@ -28,3 +63,41 @@ $("#add-book").click(function(e){
     availableBooks.append('<li>'+newBookName+'<button type="button" class="btn btn-info btn-xs checkout-book">Checkout Book</button></li>');
 
 });
+
+
+//hide buttons first
+$(".books-section .btn").hide();
+
+
+$(".books-section li").hover(
+    function(){
+        var bookItem = $(this);
+        var bookBtn = bookItem.find(".btn");
+        bookBtn.show();
+//        bookBtn.css("display","block");
+//        bookBtn.removeClass("hidden");
+    },
+    function(){
+        var bookItem = $(this);
+        var bookBtn = bookItem.find(".btn");
+        bookBtn.hide();
+//        bookBtn.css("display","none");
+//        bookBtn.addClass("hidden");
+    }
+);
+
+//
+//$(".books-section li").hover(
+//    function(){
+//        var bookItem = $(this);
+//        var bookBtn = bookItem.find(".btn");
+//
+//        bookBtn.removeClass("hidden");
+//    },
+//    function(){
+//        var bookItem = $(this);
+//        var bookBtn = bookItem.find(".btn");
+//
+//        bookBtn.addClass("hidden");
+//    }
+//);
